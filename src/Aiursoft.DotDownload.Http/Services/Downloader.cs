1d0c9e5ad12135d7894fe89d56b36c3a9625e9d0
Anduin Xue <anduin@aiursoft.com>
2023-09-13 14:42:13 +0000

    Show a progress bar.

--- src/Aiursoft.DotDownload.Http/Services/Downloader.cs
+++ src/Aiursoft.DotDownload.Http/Services/Downloader.cs
@@ -1,5 +1,6 @@
 using System.Net.Http.Headers;
 using Aiursoft.Canon;
+using Aiursoft.DotDownload.Http.Models;
 using Microsoft.Extensions.Logging;
 
 namespace Aiursoft.DotDownload.PluginFramework.Handlers.Download;
@@ -40,13 +41,13 @@ public class Downloader
         _logger.LogTrace($"Requesting {url}...");
         var response = await _httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
         var contentLength = response.Content.Headers.ContentLength ?? 0;
-        _logger.LogInformation("File length: {ContentLength}MB", contentLength/1024/1024);
-        
+        _logger.LogInformation("File length: {ContentLength}MB", contentLength / 1024 / 1024);
+
         // TODO: What if server 301 or 302?
         // TODO: If the file doesn't support multiple threads downloading?
 
         // Create the file with the specified length.
-        _logger.LogTrace($"File will be saved to {fileToWrite}...");
+        _logger.LogInformation($"File will be saved to {fileToWrite}...");
         File.Delete(fileToWrite);
         await using (var fileStream = new FileStream(fileToWrite, FileMode.Create, FileAccess.Write, FileShare.None,
                          bufferSize: 4096, useAsync: true))
@@ -55,12 +56,14 @@ public class Downloader
             fileStream.Close();
         }
 
-        long blockCount = (long)Math.Ceiling((double)contentLength / blockSize);
+        var blockCount = (long)Math.Ceiling((double)contentLength / blockSize);
         _logger.LogInformation("Blocks count: {BlockCount}", blockCount);
 
+        var bar = new ProgressBar();
+        var savedBlocks = 0;
         for (var i = 0; i < blockCount; i++)
         {
-            long offset = i * blockSize;
+            var offset = i * blockSize;
             var length = Math.Min(blockSize, contentLength - offset);
             _downloadPool.RegisterNewTaskToPool(async () =>
             {
@@ -69,9 +72,13 @@ public class Downloader
                 var fileStream = await DownloadBlockAsync(url, offset, length);
                 _writePool.QueueNew(async () =>
                     {
-                        _logger.LogTrace($"Saving block {offset / 1024 / 1024}MB to {(offset + length) / 1024 / 1024}MB on local disk...");
+                        _logger.LogTrace(
+                            $"Saving block {offset / 1024 / 1024}MB to {(offset + length) / 1024 / 1024}MB on local disk...");
                         await SaveBlockToDisk(fileStream, fileToWrite, offset);
-                        _logger.LogTrace($"Finish block {offset / 1024 / 1024}MB to {(offset + length) / 1024 / 1024}MB to save on disk.");
+                        savedBlocks++;
+                        bar.Report((double)savedBlocks / blockCount);
+                        _logger.LogTrace(
+                            $"Finish block {offset / 1024 / 1024}MB to {(offset + length) / 1024 / 1024}MB to save on disk.");
                     },
                     startTheEngine: true,
                     maxThreads: 1);
@@ -81,7 +88,8 @@ public class Downloader
         }
 
         await _downloadPool.RunAllTasksInPoolAsync(threads);
-        await _writePool.RunTasksInQueue(1);
+        await _writePool.Engine;
+        bar.Dispose();
     }
 
     private async Task<MemoryStream> DownloadBlockAsync(string url, long offset, long length)
