c730f2a17e34e050d9aed9c73149b5631c58b530
xuef <xuef@microsoft.com>
2023-09-14 07:48:23 +0000

    Trim usings.

--- src/Aiursoft.DotDownload.Http/Services/HttpBlockDownloader.cs
+++ src/Aiursoft.DotDownload.Http/Services/HttpBlockDownloader.cs
@@ -1,11 +1,5 @@
 ﻿using Aiursoft.Scanner.Abstractions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http.Headers;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Aiursoft.DotDownload.Http.Services
 {
