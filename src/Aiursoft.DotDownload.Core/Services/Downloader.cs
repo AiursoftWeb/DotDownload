eed58d04f601bea8c768febc6d9031785ae3573e
Anduin <anduin@aiursoft.com>
2024-06-27 12:37:55 +0000

    Remove ProgressBar class and update dependencies
    The ProgressBar class was deleted. Dependencies on Aiursoft.CommandFramework have been updated from 8.0.1 to 8.0.2 across multiple projects. Additionally, other minor adjustments in Downloader.cs improved code readability and detail orientation.

--- src/Aiursoft.DotDownload.Core/Services/Downloader.cs
+++ src/Aiursoft.DotDownload.Core/Services/Downloader.cs
@@ -1,44 +1,28 @@
 using Aiursoft.Canon;
-using Aiursoft.DotDownload.Core.Models;
+using Aiursoft.CommandFramework.Models;
 using Aiursoft.Scanner.Abstractions;
 using Microsoft.Extensions.Logging;
 
 namespace Aiursoft.DotDownload.Core.Services;
 
-public class Downloader : ITransientDependency
+public class Downloader(
+    WatchService watchService,
+    DiskService diskService,
+    IHttpClientFactory httpClient,
+    CanonPool downloadPool,
+    CanonQueue writePool,
+    HttpBlockDownloader httpBlockDownloader,
+    ILogger<Downloader> logger)
+    : ITransientDependency
 {
-    private readonly WatchService _watchService;
-    private readonly DiskService _diskService;
-    private readonly HttpClient _httpClient;
-    private readonly CanonPool _downloadPool;
-    private readonly CanonQueue _diskWriteQueue;
-    private readonly HttpBlockDownloader _httpBlockDownloader;
-    private readonly ILogger<Downloader> _logger;
-
-    public Downloader(
-        WatchService watchService,
-        DiskService diskService,
-        IHttpClientFactory httpClient,
-        CanonPool downloadPool,
-        CanonQueue writePool,
-        HttpBlockDownloader httpBlockDownloader,
-        ILogger<Downloader> logger)
-    {
-        _watchService = watchService;
-        _diskService = diskService;
-        _httpClient = httpClient.CreateClient(nameof(Downloader));
-        _downloadPool = downloadPool;
-        _diskWriteQueue = writePool;
-        _httpBlockDownloader = httpBlockDownloader;
-        _logger = logger;
-    }
+    private readonly HttpClient _httpClient = httpClient.CreateClient(nameof(Downloader));
 
     /// <summary>
     /// This is a C# method that downloads a file asynchronously from a given URL and saves it to a specified location on the local disk.
     /// </summary>
     /// <param name="url">Http URL to download.</param>
     /// <param name="savePath">Path to save.</param>
-    /// <param name="blockSize">Block size. It will download every blocks in parallel and save those blocks to disk with a queue.</param>
+    /// <param name="blockSize">Block size. It will download every block in parallel and save those blocks to disk with a queue.</param>
     /// <param name="threads">Max threads to download.</param>
     /// <param name="showProgressBar">If to show the progress bar.</param>
     public async Task DownloadWithWatchAsync(
@@ -48,12 +32,9 @@ public class Downloader : ITransientDependency
         int threads = 16,
         bool showProgressBar = false)
     {
-        var (elapsed, fileLength) = await _watchService.RunWithWatchAsync(async () => 
-        {
-            return await DownloadAsync(url, savePath, blockSize, threads, showProgressBar);
-        });       
-        _logger.LogTrace("Download finished in {Elapsed} seconds.", elapsed.TotalSeconds);
-        _logger.LogInformation("Download speed: {Speed}MB/s", (double)fileLength / 1024 / 1024 / elapsed.TotalSeconds);
+        var (elapsed, fileLength) = await watchService.RunWithWatchAsync(() => DownloadAsync(url, savePath, blockSize, threads, showProgressBar));       
+        logger.LogTrace("Download finished in {Elapsed} seconds.", elapsed.TotalSeconds);
+        logger.LogInformation("Download speed: {Speed}MB/s", (double)fileLength / 1024 / 1024 / elapsed.TotalSeconds);
     }
 
     private async Task<long> DownloadAsync(
@@ -64,31 +45,31 @@ public class Downloader : ITransientDependency
         bool showProgressBar)
     {
         ProgressBar? bar = null;
-        _logger.LogTrace($"Requesting {url}...");
+        logger.LogTrace($"Requesting {url}...");
         var response = await _httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
         var fileLength = response.Content.Headers.ContentLength ?? 0;
-        _logger.LogInformation("File length: {ContentLength}MB", fileLength / 1024 / 1024);
+        logger.LogInformation("File length: {ContentLength}MB", fileLength / 1024 / 1024);
 
         if (!response.Headers.AcceptRanges.Contains("bytes"))
         {
-            _logger.LogWarning("The server doesn't support multiple threads downloading. Using single block...");
+            logger.LogWarning("The server doesn't support multiple threads downloading. Using single block...");
             showProgressBar = false;
             threads = 1;
             blockSize = fileLength;
         }
 
         savePath = PathExtensions.GetFilePathToSave(savePath, url);
-        _logger.LogInformation($"File will be saved to {savePath}...");
+        logger.LogInformation($"File will be saved to {savePath}...");
 
         if (File.Exists(savePath))
         {
-            _logger.LogWarning("File already exists. Deleting...");
+            logger.LogWarning("File already exists. Deleting...");
             File.Delete(savePath);
         }
 
-        _diskService.CreateFileAndAllocateSpace(savePath, fileLength);
+        diskService.CreateFileAndAllocateSpace(savePath, fileLength);
         var blockCount = (long)Math.Ceiling((double)fileLength / blockSize);
-        _logger.LogInformation("Blocks count: {BlockCount}", blockCount);
+        logger.LogInformation("Blocks count: {BlockCount}", blockCount);
 
         if (showProgressBar) bar = new ProgressBar();
         var savedBlocks = 0;
@@ -96,30 +77,32 @@ public class Downloader : ITransientDependency
         {
             var offset = i * blockSize;
             var length = Math.Min(blockSize, fileLength - offset);
-            _downloadPool.RegisterNewTaskToPool(async () =>
+            downloadPool.RegisterNewTaskToPool(async () =>
             {
-                _logger.LogTrace(
+                logger.LogTrace(
                     $"Starting download with offset: {offset / 1024 / 1024}MB, length {length / 1024 / 1024}MB, totally {fileLength / 1024 / 1024}MB");
-                var block = await _httpBlockDownloader.DownloadBlockAsync(url, offset, length);
-                _diskWriteQueue.QueueNew(async () =>
+                var block = await httpBlockDownloader.DownloadBlockAsync(url, offset, length);
+                writePool.QueueNew(async () =>
                 {
-                    _logger.LogTrace(
+                    logger.LogTrace(
                         $"Saving block {offset / 1024 / 1024}MB to {(offset + length) / 1024 / 1024}MB on local disk...");
-                    await _diskService.SaveBlockToDisk(block, savePath, offset);
+                    await diskService.SaveBlockToDisk(block, savePath, offset);
                     savedBlocks++;
+                    
+                    // ReSharper disable once AccessToDisposedClosure
                     bar?.Report((double)savedBlocks / blockCount);
-                    _logger.LogTrace(
+                    logger.LogTrace(
                         $"Finish block {offset / 1024 / 1024}MB to {(offset + length) / 1024 / 1024}MB to save on disk.");
                 },
                     startTheEngine: true,
                     maxThreads: 1); // Only one thread to write to disk.
-                _logger.LogTrace(
+                logger.LogTrace(
                     $"Finished download with offset: {offset / 1024 / 1024}MB, length {length / 1024 / 1024}MB, totally {fileLength / 1024 / 1024}MB");
             });
         }
 
-        await _downloadPool.RunAllTasksInPoolAsync(threads);
-        await _diskWriteQueue.Engine; // Make sure the write pool is finished.
+        await downloadPool.RunAllTasksInPoolAsync(threads);
+        await writePool.Engine; // Make sure the write pool is finished.
         bar?.Dispose();
 
         return fileLength;
